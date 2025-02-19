c45904708260c02fcb22e1f7a33839025a05f3e7
kimtaewoo <ktw2000111@naver.com>
2025-02-19 17:06:54 +0900

    2-19 17:06

--- Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
+++ Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
@@ -1,8 +1,14 @@
 package com.example.spring06.dto;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
 import lombok.Setter;
 
+/*
+ * type 에 별칭을 부여하고 Mapper.xml 문서에서 별칭을 이용해서 parameterType 과 resultType 설정 가능
+ */
+@Alias("memberDto")
 @Getter
 @Setter
 public class MemberDto {
---
+++ Spring06_Service/src/test/java/com/example/spring06/Spring06ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
