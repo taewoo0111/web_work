1c5d3b26302268c16bf899a4d7cb62f3a7e8b02c
kimtaewoo <ktw2000111@naver.com>
2025-02-19 17:07:21 +0900

    2-19 17:07

---
+++ Spring07_FileUpload/src/main/java/com/example/spring07/Spring07FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07FileUploadApplication.class, args);
+	}
+
+}
