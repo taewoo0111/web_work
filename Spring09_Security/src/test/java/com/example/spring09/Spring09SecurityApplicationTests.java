673ddf474b47e989e7d6860d3ce2683470af6ef0
kimtaewoo <ktw2000111@naver.com>
2025-02-20 18:04:07 +0900

    2-20 18:04

---
+++ Spring09_Security/src/test/java/com/example/spring09/Spring09SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
