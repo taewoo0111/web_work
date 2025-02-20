f2917977359f7093b486b3e50940cac5d7c7ad5d
kimtaewoo <ktw2000111@naver.com>
2025-02-20 15:54:10 +0900

    2-20 15:54

---
+++ Spring08_Java/src/test/java/com/example/spring08/Spring08JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
