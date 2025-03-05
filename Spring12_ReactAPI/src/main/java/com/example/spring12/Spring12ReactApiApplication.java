4e51752d7dac76ec25de76a19f95e6fd59c33118
kimtaewoo <ktw2000111@naver.com>
2025-03-06 08:05:49 +0900

    3-6 08:05

---
+++ Spring12_ReactAPI/src/main/java/com/example/spring12/Spring12ReactApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12ReactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring12ReactApiApplication.class, args);
+	}
+
+}
---
+++ Spring12_ReactAPI/src/test/java/com/example/spring12/Spring12ReactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12ReactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
