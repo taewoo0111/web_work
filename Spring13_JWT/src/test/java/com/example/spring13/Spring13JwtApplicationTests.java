4b0fdec59e2324c1062c83079ca1eb0e0cf5ca55
kimtaewoo <ktw2000111@naver.com>
2025-03-10 16:49:04 +0900

    3-10 16:49

---
+++ Spring13_JWT/src/test/java/com/example/spring13/Spring13JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring13JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
