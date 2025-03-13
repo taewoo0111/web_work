379ecbc1729b1524c3958be9cca3bccb583e7200
kimtaewoo <ktw2000111@naver.com>
2025-03-13 15:45:06 +0900

    3-13 15:45

---
+++ Spring15_FinalAPI/src/main/java/com/example/spring15/Spring15FinalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring15FinalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring15FinalApiApplication.class, args);
+	}
+
+}
---
+++ Spring15_FinalAPI/src/test/java/com/example/spring15/Spring15FinalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring15FinalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
