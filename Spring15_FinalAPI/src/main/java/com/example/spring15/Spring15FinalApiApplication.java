f3df491a78243499f76ef172b9f91941fdf06123
kimtaewoo <ktw2000111@naver.com>
2025-03-13 17:55:45 +0900

    3-13 17:55

--- Spring15_FinalAPI/src/main/java/com/example/spring15/Spring15FinalApiApplication.java
+++ Spring15_FinalAPI/src/main/java/com/example/spring15/Spring15FinalApiApplication.java
@@ -2,8 +2,11 @@ package com.example.spring15;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:custom.properties")
 public class Spring15FinalApiApplication {
 
 	public static void main(String[] args) {
