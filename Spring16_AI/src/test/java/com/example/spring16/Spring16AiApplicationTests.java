4e8040f63d95f81f7d5a5d09d500c9b3c6b8e36b
kimtaewoo <ktw2000111@naver.com>
2025-03-20 12:45:53 +0900

    3-20 12:45

---
+++ Spring16_AI/src/test/java/com/example/spring16/Spring16AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring16AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
